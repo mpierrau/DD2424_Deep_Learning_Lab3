Sorted out some bugs. Gradients for W are still wrong tho.

K_NN_layer_class.py
@@ -1,6 +1,7 @@
 # Base layer class
 import numpy as np 
 from numpy import random
+import copy
 
 class Layer:
 
@@ -37,12 +38,12 @@ class FCLayer(Layer):
         self.normalize = normalize
 
         # Pars for batch normalization
-        self.mu = None
-        self.v = None
+        self.batch_mu = None
+        self.batch_v = None
 
-        self.alpha = alpha # Weight for mu_avg and v_avg in weighted average
-        self.mu_avg = None
-        self.v_avg = None
+        self.alpha = alpha # Weight for avg_mu and avg_v in weighted average
+        self.avg_mu = None
+        self.avg_v = None
 
         self.beta = np.zeros((output_size,1))
         self.gamma = np.ones((output_size,1))
@@ -57,7 +58,12 @@ class FCLayer(Layer):
                     
         self.W = self.init_func(in_dim=self.nCols,out_dim=self.nRows) if W is None else W
         self.b = np.zeros((self.nRows,1)) if b is None else b
-    
+
+        self.normalize_fw_func = None
+        self.normalize_bw_func = None
+        self.normalize_grad_func = None
+        self.normalize_update_pars_func = None
+
         if self.normalize == True:
             self.normalize_fw_func = self.batchNormFProp
             self.normalize_bw_func = self.batchNormBProp
@@ -69,32 +75,41 @@ class FCLayer(Layer):
             self.normalize_grad_func = lambda *args : None
             self.normalize_update_pars_func = lambda eta : [self.gamma,self.beta]
 
-    def forward_pass(self, input_data,prediction=False):
-
+    def forward_pass(self, input_data,prediction=False,debug=False):
         self.input = input_data
         self.batchSize = input_data.shape[1]
-
         self.unnorm_score = np.dot(self.W,self.input) + self.b
+        
+        if self.avg_mu is None:
+            avg_mu = self.unnorm_score.mean(axis=1)
+            self.avg_mu = avg_mu
+        if self.avg_v is None:
+            avg_v = self.unnorm_score.var(axis=1,ddof=0)
+            self.avg_v = avg_v
 
         if prediction==True:
-            mu = self.mu_avg
-            v = self.v_avg
+            mu = self.avg_mu
+            v = self.avg_v
         else:
-            self.mu = self.unnorm_score.mean(axis=1)
-            self.v = self.unnorm_score.var(axis=1,ddof=0) # Biased estimator
+            self.batch_mu = self.unnorm_score.mean(axis=1)
+            self.batch_v = self.unnorm_score.var(axis=1,ddof=0) # Biased estimator
             
-            # For very first batch we initialize mu_avg as mu and v_avg as v, else weighted average.
-            self.mu_avg = self.mu if self.mu_avg is None else np.average([self.mu_avg,self.mu],axis=0,weights=(self.alpha,1-self.alpha))
-            self.v_avg = self.v if self.v_avg is None else np.average([self.v_avg,self.v],axis=0,weights=(self.alpha,1-self.alpha))
+            # For very first batch we initialize avg_mu as mu and avg_v as v, else weighted average.
+            avg_mu = np.average([self.avg_mu,self.batch_mu],axis=0,weights=(self.alpha,1-self.alpha))
+            avg_v = np.average([self.avg_v,self.batch_v],axis=0,weights=(self.alpha,1-self.alpha))
+            
+            self.avg_mu = avg_mu
+            self.avg_v = avg_v
 
-            mu = self.mu
-            v = self.v
-
+            mu = self.batch_mu
+            v = self.batch_v
+        
         self.normalize_fw_func(mu,v)
 
         s_tilde = self.gamma*self.norm_score + self.beta
-        self.output = s_tilde
         
+        self.output = s_tilde
+
         return self.output
     
     def backward_pass(self, G, eta):
@@ -102,7 +117,7 @@ class FCLayer(Layer):
         """ This here pass has a packpropagated gradient for dJdW which originates both from the hidden layer
          and from the regularization term. 
          We also compute and update the W and b parameter here """
-        
+
         self.compute_norm_grads(G)
 
         G *= self.gamma
@@ -112,9 +127,9 @@ class FCLayer(Layer):
         newG = np.dot(self.W.T,G)
 
         self.compute_grads(G)
+
         self.update_pars(eta)
 
-        # Return gradient to pass on upwards
         return newG
     
     def compute_grads(self, G):
@@ -139,7 +154,6 @@ class FCLayer(Layer):
 
         # Update scale and shift parameters
         newGamma , newBeta = self.normalize_update_pars_func(eta)
-
         self.gamma = newGamma
         self.beta = newBeta
 
@@ -147,23 +161,16 @@ class FCLayer(Layer):
         dJdgamma = np.sum((G * self.norm_score),axis=1) / self.batchSize
         dJdbeta = np.sum(G, axis=1) / self.batchSize
 
-        self.gradGamma = dJdgamma
-        self.gradBeta = dJdbeta
+        self.gradGamma = dJdgamma.reshape((len(dJdgamma),1))
+        self.gradBeta = dJdbeta.reshape((len(dJdbeta),1))
 
-    def batchNormFProp(self,*argv):
-
-        if len(argv) == 0:
-            mu = self.mu
-            v = self.v
-        else:
-            mu = argv[0]
-            v = argv[1]
-        
+    def batchNormFProp(self,mu,v):
         eps = np.finfo(float).eps
-        sigma = v
-        sigma += eps
-        sigma **= -.5
-        norm_score = np.diag(sigma)@(self.unnorm_score - mu[:,None])
+
+        sigma = v**(-.5)
+
+        norm_score = np.diag(sigma)@(self.unnorm_score - mu[:,None]) + eps
+        
         self.norm_score = norm_score
     
     def batch_Non_NormFProp(self,*argv):
@@ -179,13 +186,13 @@ class FCLayer(Layer):
 
         eps = np.finfo(float).eps
 
-        sig1 = ((self.v + eps)**(-.5))
-        sig2 = ((self.v + eps)**(-1.5))
+        sig1 = ((self.batch_v + eps)**(-.5))
+        sig2 = ((self.batch_v + eps)**(-1.5))
 
         G1 = G*sig1[:,None]
         G2 = G*sig2[:,None]
 
-        D = self.unnorm_score - self.mu[:,None]
+        D = self.unnorm_score - self.batch_mu[:,None]
         c = np.sum(G2*D,axis=1)
 
         G = G1 - (np.sum(G1,axis=1) / N)[:,None] - (D*c[:,None] / N )
@@ -196,11 +203,16 @@ class FCLayer(Layer):
         return G
 
     def updateParsNorm(self,eta):
-        newGamma = self.gamma - eta * self.gradGamma[:,None]
-        newBeta = self.beta - eta * self.gradBeta[:,None]
+        newGamma = self.gamma - eta * self.gradGamma
+        newBeta = self.beta - eta * self.gradBeta
         
         return newGamma , newBeta
 
+    def get_batch_v(self):
+        return self.batch_v
+
+    def get_avg_v(self):
+        return self.avg_v
 
 class ActLayer(Layer):
 
@@ -208,7 +220,7 @@ class ActLayer(Layer):
         self.act_func = act_func
         self.name = "Activation Layer" if name is None else name
 
-    def forward_pass(self, input_data,prediction=False):
+    def forward_pass(self, input_data,prediction=False,debug=False):
         self.input = input_data
         self.output = self.act_func(self.input)
         # Apply batch normalization here

norm_test.py
@@ -23,14 +23,23 @@ Xin = [Xtrain,Xval]
 Yin = [Ytrain,Yval]
 yin = [ytrain,yval]
 
-d , N = Xtrain.shape
+Xred , Yred , yred = reduceDims(Xtrain,Ytrain,ytrain,10,5)
+
+d , N = Xred.shape
+k = Yred.shape[0]
+
+"""d , N = Xtrain.shape
 k = np.shape(Ytrain)[0]
+"""
+dims = [15,10]
 
-dims = [50,20]
-
-net = Network(normalize=False)
+net = Network(normalize=True,alpha=0.5)
 net.build_layers(d,k,dims)
-net.fit(Xin,Yin,yin,2,50,100,[1e-5,1e-1],.001,10,seed=1337)
+
+net.forward_prop(Xred)
+net.backward_prop(Yred,1e-2)
+
+#net.fit(Xin,Yin,yin,2,50,100,[1e-5,1e-1],.001,10,seed=1337)
 
 
 netNorm = Network(normalize=True)
@@ -47,10 +56,3 @@ net.accuracy["Test"]
 
 netNorm.compute_accuracy(Xtest,ytest,key="Test")
 netNorm.accuracy["Test"]
-
-numbers = [[1, 2, 3],[4,5,6]]
-letters = [["A", "B", "C"],["D","E","F"]]
-
-for numbers_list, letters_list in zip(numbers, letters):
-    for numbers_value , letters_value in zip(numbers_list, letters_list):
-        print(numbers_value, letters_value)

numerical_grads.py
@@ -3,10 +3,11 @@ import copy
 from K_NN_funcs import L2_cost , cross_entropy , cross_entropy_prime , relu
 from K_NN_network_class import Network
 from K_NN_layer_class import FCLayer , ActLayer
+import sys
 
 """ Test function for numerical gradient computation """
 
-def testGrads(X, Y, y, layerDims, lamda, h, init_func, nBatch=None, mu=0, sig=0.01, fast=True, debug=False, eta=1e-5, burnIn=10,normalize=False):
+def testGrads(X, Y, y, layerDims, lamda, h, init_func, nBatch=None, mu=0, sig=0.01, fast=True, debug=False, eta=1e-5, burnIn=10,normalize=False,alpha=0.9):
     # Compares results of analytically computed
     # gradients to numerical approximations to
     # ensure correctness.
@@ -22,76 +23,75 @@ def testGrads(X, Y, y, layerDims, lamda, h, init_func, nBatch=None, mu=0, sig=0.
     
     print("Building anNet : \n")
 
-    anNet = Network(normalize=normalize)
+    anNet = Network(normalize=normalize,alpha=alpha)
     anNet.build_layers(d,k,layerDims)
     
-    print("Burning in %d steps..." % burnIn)
+    print("\nBurning in %d steps...\n" % burnIn)
     
     anNet.nBatch = nBatch
     anNet.eta = [eta,eta]
     anNet.lamda = lamda
-
-    for _ in range(burnIn-1):
-        anNet.forward_prop(X)
+    for i in range(burnIn-1):
+        print("Burn in step ", (i+1))
+        anNet.forward_prop(X,debug=False)
         anNet.backward_prop(Y, anNet.eta[0])
 
-    print("number of steps after burn in: ", len(anNet.loss["Training"]))
-
+    
     anNet.compute_loss(Y)
     anNet.compute_cost()
+    
+    print("Burn in done")
+
+    anNet.eta = [0,0]
+
+    print("Copying net for test...")
 
     test_net = copy.deepcopy(anNet)
 
-    print("Computing grads using %s algorithm..." % ("fast but inaccurate" if fast else "slow but accurate"))
-
-    numGrads , numNet = computeGradsNum(test_net,X,Y,y,h,fast,normalize)
-
-    anNet.forward_prop(X)
+    print("Computing grads using %s algorithm...\n" % ("fast but inaccurate" if fast else "slow but accurate"))
+    numGrads , numNet = computeGradsNum(test_net,X,Y,y,h,fast)
+    print("Numerical grads computed!\n")
+    anNet.forward_prop(X,debug=False)
     anNet.backward_prop(Y, anNet.eta[0])
 
-    grad_names = ["W", "b"]
-    Wgrads = []
-    bgrads = []
+    anGrads = {"W":{},"b":{}}
+    
+    if normalize:
+        anGrads["beta"] = {}
+        anGrads["gamma"] = {}
 
-    if normalize==True:
-        betagrads = []
-        gammagrads = []
-        grad_names.append("beta")
-        grad_names.append("gamma")
-
-        for layer in anNet.layers:
+        for i,layer in enumerate(anNet.layers):
             if type(layer) == FCLayer:
-                Wgrads.append(layer.gradW)
-                bgrads.append(layer.gradb)
-                betagrads.append(layer.gradBeta)
-                gammagrads.append(layer.gradGamma)
-        
-        anGrads = [Wgrads,bgrads,betagrads,gammagrads]
+                anGrads["W"][i] = layer.gradW
+                anGrads["b"][i] = layer.gradb
+                anGrads["beta"][i] = layer.gradBeta
+                anGrads["gamma"][i] = layer.gradGamma
 
+        relErrs = {"W":{},"b":{},"beta":{},"gamma":{}}
     else:
         
-        for layer in anNet.layers:
+        for i,layer in enumerate(anNet.layers):
             if type(layer) == FCLayer:
-                print("Adding grads from layer %d" % layer.layerIdx)
-                Wgrads.append(layer.gradW)
-                bgrads.append(layer.gradb)
-        
-        anGrads = [Wgrads,bgrads]
+                anGrads["W"][i] = layer.gradW
+                anGrads["b"][i] = layer.gradb
 
-    
-    relErrs = []
+        relErrs = {"W":{},"b":{}}
 
-    for i , (anPar , numPar) in enumerate(zip(anGrads,numGrads)):
-        print("Parameter : %s" % grad_names[i])
-        for j , (anGrad , numGrad) in enumerate(zip(anPar , numPar)):
-            print("Layer : ", j)
-            err = relErr(anGrad,numGrad)
-            print("Relative error : ", err)
-            relErrs.append(err)
+
+    for par in anGrads.keys():
+        for lay in anGrads[par].keys():
+            err = relErr(anGrads[par][lay],numGrads[par][lay])
+            relErrs[par][lay] = err
     
+    maxerror = -sys.maxsize - 1
+
+    for key in relErrs.keys():
+        for idx in relErrs[key].keys():
+            tmpVal = relErrs[key][idx]
+            maxerror = tmpVal if tmpVal > maxerror else maxerror
     
-    print("\nLargest relative error: %e" % (np.max(relErrs)))
-    maxerror = np.max(relErrs)
+    print("\nLargest relative error: %e" % (maxerror))
+
     if maxerror > 1e-2:
         print("Probably issue with gradient")
     elif maxerror > 1e-4:
@@ -104,13 +104,13 @@ def testGrads(X, Y, y, layerDims, lamda, h, init_func, nBatch=None, mu=0, sig=0.
     return relErrs , anGrads , numGrads , anNet , numNet
 
 
-def computeGradsNum(net, X, Y, y, h=1e-6, fast=False, normalize=False):
+
+def computeGradsNum(net, X, Y, y, h=1e-6, fast=False):
         """ Uses finite or centered difference approx depending on fast boolean 
             Good practice: let net burn in a few steps before computing grads"""
-
-        grads_W = []
-        grads_b = []
         
+        numGrads = {"W":{},"b":{}}
+
         if fast:
             print("Using finite difference method")
             approx_func = lambda el,layer_idx,el_idx_i,el_idx_j,c : finite_diff(net,X,Y,el,layer_idx,el_idx_i,el_idx_j,c,h)
@@ -125,64 +125,59 @@ def computeGradsNum(net, X, Y, y, h=1e-6, fast=False, normalize=False):
         biases = net.get_biases()
 
         for k,lay_idx in enumerate(biases.keys()):
-            grads_b.append(np.zeros(biases[lay_idx].shape))
+            numGrads["b"][2*k] = np.zeros(biases[lay_idx].shape)
+            #grads_b.append(np.zeros(biases[lay_idx].shape))
             
-            for i in range(grads_b[k].shape[0]):
+            for i in range(numGrads["b"][2*k].shape[0]):
                 grad_approx = approx_func("b",lay_idx,i,0,c)
-                grads_b[k][i,0] = grad_approx
+                numGrads["b"][2*k][i,0] = grad_approx
 
-            net.layers[lay_idx].gradb = grads_b[k]
+            net.layers[lay_idx].gradb = numGrads["b"][2*k]
         
 
         # Compute grads for all W
         weights = net.get_weights()
 
         for k,lay_idx in enumerate(weights.keys()):
-            grads_W.append(np.zeros(weights[lay_idx].shape))
+            numGrads["W"][2*k] = np.zeros(weights[lay_idx].shape)
+            #grads_W.append(np.zeros(weights[lay_idx].shape))
 
-            for i in range(grads_W[k].shape[0]):
-                for j in range(grads_W[k].shape[1]):
+            for i in range(numGrads["W"][2*k].shape[0]):
+                for j in range(numGrads["W"][2*k].shape[1]):
                     grad_approx = approx_func("W", lay_idx, i, j, c)
-                    grads_W[k][i,j] = grad_approx
+                    numGrads["W"][2*k][i,j] = grad_approx
         
-            net.layers[lay_idx].gradW = grads_W[k]
+            net.layers[lay_idx].gradW = numGrads["W"][2*k]
 
-        grads = [grads_W , grads_b]
-
-        if normalize:
-            grads_gamma = []
-            grads_beta = []
+        if net.normalize:
+            numGrads["gamma"] = {}
+            numGrads["beta"] = {}
 
             # Compute grads for all gamma
             gammas = net.get_gammas()
 
             for k,lay_idx in enumerate(gammas.keys()):
-                grads_gamma.append(np.zeros(gammas[lay_idx].shape))
+                numGrads["gamma"][2*k] = np.zeros(gammas[lay_idx].shape)
+                #grads_gamma.append(np.zeros(gammas[lay_idx].shape))
                 
-                for i in range(grads_gamma[k].shape[0]):
+                for i in range(numGrads["gamma"][2*k].shape[0]):
                     grad_approx = approx_func("gamma",lay_idx,i,0,c)
-                    grads_gamma[k][i,0] = grad_approx
+                    numGrads["gamma"][2*k][i,0] = grad_approx
 
-                net.layers[lay_idx].gradGamma = grads_gamma[k]
-
-            grads.append(grads_gamma)
+                net.layers[lay_idx].gradGamma = numGrads["gamma"][2*k]
 
             # Compute grads for all beta
             betas = net.get_betas()
 
             for k,lay_idx in enumerate(betas.keys()):
-                grads_beta.append(np.zeros(betas[lay_idx].shape))
-                
-                for i in range(grads_beta[k].shape[0]):
+                numGrads["beta"][2*k] = np.zeros(betas[lay_idx].shape)
+                for i in range(numGrads["beta"][2*k].shape[0]):
                     grad_approx = approx_func("beta",lay_idx,i,0,c)
-                    grads_beta[k][i,0] = grad_approx
+                    numGrads["beta"][2*k][i,0] = grad_approx
 
-                net.layers[lay_idx].gradBeta = grads_beta[k]
-
-            grads.append(grads_beta)
-            
+                net.layers[lay_idx].gradBeta = numGrads["beta"][2*k]            
         
-        return grads , net
+        return numGrads , net
 
 
 
@@ -190,7 +185,7 @@ def finite_diff(net,X,Y,el,layer_idx,el_idx_i,el_idx_j,c,h):
 
     update_el(net,el,layer_idx,el_idx_i,el_idx_j,h)
 
-    net.forward_prop(X)
+    net.forward_prop(X,prediction=True)
     net.compute_loss(Y)
 
     net.compute_cost()
@@ -208,16 +203,16 @@ def finite_diff(net,X,Y,el,layer_idx,el_idx_i,el_idx_j,c,h):
 def centered_diff(net,X,Y,el,layer_idx,el_idx_i,el_idx_j,h):
     
     update_el(net,el,layer_idx,el_idx_i,el_idx_j,-h)
-
-    net.forward_prop(X)
+        
+    net.forward_prop(X,prediction=True)
     net.compute_loss(Y)
     net.compute_cost()
-    
+
     c1 = net.cost["Training"][-1]
 
     update_el(net,el,layer_idx,el_idx_i,el_idx_j,2*h)
     
-    net.forward_prop(X)
+    net.forward_prop(X,prediction=True)
     net.compute_loss(Y)
     net.compute_cost()
 
@@ -231,19 +226,35 @@ def centered_diff(net,X,Y,el,layer_idx,el_idx_i,el_idx_j,h):
     return grad_approx
 
 def update_el(net,el,layer_idx,el_idx_i,el_idx_j,h):
-        if el == "b":
-            net.layers[layer_idx].b[el_idx_i,el_idx_j] += h
-        elif el == "W":
-            net.layers[layer_idx].W[el_idx_i,el_idx_j] += h
-        elif el == "gamma":
-            net.layers[layer_idx].gamma[el_idx_i,el_idx_j] += h
-        elif el == "beta":
-            net.layers[layer_idx].beta[el_idx_i,el_idx_j] += h
-        else:
-            pass
+    if el == "b":
+        net.layers[layer_idx].b[el_idx_i,el_idx_j] += h
+    elif el == "W":
+        net.layers[layer_idx].W[el_idx_i,el_idx_j] += h
+    elif el == "gamma":
+        net.layers[layer_idx].gamma[el_idx_i,el_idx_j] += h
+    elif el == "beta":
+        net.layers[layer_idx].beta[el_idx_i,el_idx_j] += h
+    else:
+        pass
 
-def relErr(Wan,Wnum,eps=1e-10):
+def reset_entries(net,el,layer_idx,el_idx_i,el_idx_j,h):
+    if el == "b":
+        net.layers[layer_idx].b[el_idx_i,el_idx_j] -= h
+    elif el == "W":
+        net.layers[layer_idx].W[el_idx_i,el_idx_j] -= h
+    elif el == "gamma":
+        net.layers[layer_idx].gamma[el_idx_i,el_idx_j] -= h
+    elif el == "beta":
+        net.layers[layer_idx].beta[el_idx_i,el_idx_j] -= h
+    else:
+        pass
+
+def relErr(Wan,Wnum,eps=1e-17):
     # Computes mean relative error between Jacobians Wan and Wnum
     #return np.mean(np.abs(Wan - Wnum)/np.maximum(np.abs(Wan),np.abs(Wnum)))
+    if np.shape(Wan) != np.shape(Wnum):
+        print("Wan and Wnum have different dimensions!")
+        print("Wan: %s != Wnum: %s" % (np.shape(Wan),np.shape(Wnum)))
+
     relErr = np.mean(np.abs(Wan - Wnum)/np.maximum(eps,(np.abs(Wan) + np.abs(Wnum))))
     return relErr